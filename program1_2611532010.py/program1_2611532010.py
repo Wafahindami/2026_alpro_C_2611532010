c6848bad6c7d791db730dc9dc37a2fa79ab252b4
Wafahindami <Wafahindami37@gmail.com>
2026-09-03 12:38:03 +0700

    first commit

---
+++ program1_2611532010.py/program1_2611532010.py
@@ -0,0 +1,13 @@
+# program pertama
+"""
+komentar
+dua baris atau lebih
+pertemuan 1 praktikum alpro
+"""
+print ("hello world")
+print()
+print("Departemen Informatika")
+print ("Universitas Andalas")
+print ("/ \\ // \\\\ /// \\\\\\")
+print ("program ini" + "dibuat oleh a\\n" 
+       + "Wafa hindami dan 2611532010")

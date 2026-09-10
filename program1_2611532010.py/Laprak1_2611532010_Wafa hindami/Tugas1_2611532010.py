263c6b73e25233202a68ad1a12cec432931ab794
Wafa <whstudio478@gmail.com>
2026-09-10 20:36:22 +0700

    Refactor Tugas1_2611532010.py for clarity

--- program1_2611532010.py/Laprak1_2611532010_Wafa hindami/Tugas1_2611532010.py
+++ program1_2611532010.py/Laprak1_2611532010_Wafa hindami/Tugas1_2611532010.py
@@ -1,9 +1,5 @@
 # Tugas 1 melakukan program sederhana
 print("Hello World")
 print("selamat pagi dunia")
-nama = "wafa"
-umur = 18
 
-print(nama)
-print(umur)
 print("baris 1 \n baris 2  \n baris 3")

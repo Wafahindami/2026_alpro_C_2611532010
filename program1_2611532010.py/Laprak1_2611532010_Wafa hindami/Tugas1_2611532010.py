4956d899f2b6a1829ae3edf4056b14078dfa7745
Wafa <whstudio478@gmail.com>
2026-09-10 20:31:35 +0700

    Add files via upload

---
+++ program1_2611532010.py/Laprak1_2611532010_Wafa hindami/Tugas1_2611532010.py
@@ -0,0 +1,9 @@
+# Tugas 1 melakukan program sederhana
+print("Hello World")
+print("selamat pagi dunia")
+nama = "wafa"
+umur = 18
+
+print(nama)
+print(umur)
+print("baris 1 \n baris 2  \n baris 3")

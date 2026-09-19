f13d2fe650ea65ea4da8628e7dd552e51fd6f8b0
Wafahindami <Wafahindami37@gmail.com>
2026-09-19 12:21:51 +0700

    pekan 3

--- Pekan3_2611532010/aritmatika_2611532010.py
+++ Pekan3_2611532010/aritmatika_2611532010.py
@@ -9,36 +9,36 @@ angka2_2010 = int(input("Input angka-2: "))
 
 # Penjumlahan
 hasil_2010 = angka1_2010 + angka2_2010
-print("/nOperator Penjumlahan")
+print("\nOperator Penjumlahan")
 print("hasil =" , hasil_2010)
 
 # Pengurangan
 hasil_2010 = angka1_2010 - angka2_2010
-print("/nOperator Pengurangan")
+print("\nOperator Pengurangan")
 print("hasil =" , hasil_2010)
 
 # Perkalian
 hasil_2010 = angka1_2010 * angka2_2010
-print("/nOperator Perkalian")
+print("\nOperator Perkalian")
 print("hasil =" , hasil_2010)
 
 # Pembagian, pembagian bulat, dan sisa bagi
 if angka2_2010 != 0:
     hasil_2010 = angka1_2010 / angka2_2010
-    print("/nOperator Pembagian")
+    print("\nOperator Pembagian")
     print("hasil =" , hasil_2010)
 
     hasil_2010 = angka1_2010 // angka2_2010
-    print("/nOperator Pembagian bulat")
+    print("\nOperator Pembagian bulat")
     print("hasil =" , hasil_2010)
 
     hasil_2010 = angka1_2010 % angka2_2010
-    print("/nOperator Sisa bagi")
+    print("\nOperator Sisa bagi")
     print("hasil =" , hasil_2010)
 else:
     print("angka kedua tidak boleh bernilai 0,")
 
 # Pangkat
 hasil_2010 = angka1_2010 ** angka2_2010
-print("/nOperator Pangkat")
+print("\nOperator Pangkat")
 print("hasil =" , hasil_2010)
